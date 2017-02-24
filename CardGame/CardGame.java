96bd0e1766e953183c6cfb43e3c6e39f6800d675
sroccor <seamus.rosenberg@gmail.com>
2017-02-23 19:33:06 -0800

    Add files via upload

--- CardGame/CardGame.java
+++ CardGame/CardGame.java
@@ -10,7 +10,8 @@ public class CardGame {
     private int playerScore;
 
     private int oppoScore;
-//constructs a card game with 2 standard size hands and a standard size deck.
+
+    //constructs a card game with 2 standard size hands and a standard size deck.
     public CardGame(){
         this.handOne = new Hand();
         this.handTwo = new Hand();
@@ -18,7 +19,8 @@ public class CardGame {
         this.playerScore = 0;
         this.oppoScore = 0;
     }
-//constructs a card game with 2 hands of the provided size and a deck containing the given number of copies of each possible card.
+
+    //constructs a card game with 2 hands of the provided size and a deck containing the given number of copies of each possible card.
     public CardGame( int handSize, int copiesPerDeck ){
         this.handOne = new Hand(handSize);
         this.handTwo = new Hand(handSize);
@@ -26,27 +28,29 @@ public class CardGame {
         this.playerScore = 0;
         this.oppoScore = 0;
     }
-//plays out a single round of the game, drawing new hands for each player, scoring them, declaring the round's winner, and updating the overall scores.
+
+    //plays out a single round of the game, drawing new hands for each player, scoring them, declaring the round's winner, and updating the overall scores.
     public void playRound(){
         int roundScoreOne = 0;
-        int roundScoreTwo = 0;
+        int roundScoreTwo = 0 ;
         this.handOne.drawNewHandFrom(gameDeck);
         this.handTwo.drawNewHandFrom(gameDeck);
-        this.scoreHand(handOne, handTwo);
-        this.roundScoreOne = score;
-        this.scoreHand(handTwo, handOne);
-        this.roundScoreTwo = score;
+        roundScoreOne = this.scoreHand(handOne, handTwo);
+        roundScoreTwo = this.scoreHand(handTwo, handOne);
         if (roundScoreOne > roundScoreTwo) {
             this.playerScore = roundScoreOne - roundScoreTwo;
+            System.out.println("Player One Wins Round");
         } else if (roundScoreTwo > roundScoreOne) {
             this.oppoScore = roundScoreTwo - roundScoreOne;
+            System.out.println("Player Two Wins Round");
         }
     }
-//returns the score of the first hand when compared to the second.
-// Face cards (Kings, Queens, and Jacks) are worth 10 points if 2 other cards of the same suit as the face card are in the player's hand.
-// Cards of rank 10 through 6 are worth 8 points if another card in the player's hand has a rank one higher or lower than card being scored.
-// Cards of rank 5 through 2 are worth 7 points if the opponent's hand contains a face card.
-// Cards of rank Ace are worth 15 points, regardless of the other cards in either player's hand.
+
+    //returns the score of the first hand when compared to the second.
+    // Face cards (Kings, Queens, and Jacks) are worth 10 points if 2 other cards of the same suit as the face card are in the player's hand.
+    // Cards of rank 10 through 6 are worth 8 points if another card in the player's hand has a rank one higher or lower than card being scored.
+    // Cards of rank 5 through 2 are worth 7 points if the opponent's hand contains a face card.
+    // Cards of rank Ace are worth 15 points, regardless of the other cards in either player's hand.
     public static int scoreHand( Hand scoredHand, Hand otherHand ){
         int score = 0;
         for (int i = 0; i < scoredHand.getHandSize(); i++) {
@@ -67,23 +71,43 @@ public class CardGame {
             }
         } return score;
     }
-//returns the first player's score.
+
+    //returns the first player's score.
     public int getPlayerScore(){
         return this.playerScore;
     }
-//returns the second player's score.
+
+    //returns the second player's score.
     public int getOpponentScore(){
         return this.oppoScore;
     }
-//returns whether the first player is winning.
+
+    //returns whether the first player is winning.
     public boolean amIWinning(){
-        throw new UnsupportedOperationException();
+        if (playerScore > oppoScore) {
+            return true;
+        } else {
+            return false;
+        }
     }
-//returns whether the game is over based upon both players' scores and the cards remaining in the deck.
+
+    //returns whether the game is over based upon both players' scores and the cards remaining in the deck.
     public boolean isGameOver(){
-        throw new UnsupportedOperationException();
+        if (playerScore == oppoScore && playerScore == 50) {
+            System.out.println("The Game ends in a Tie");
+            return true;
+        } else if (playerScore == 50) {
+            System.out.println("Player One wins the Game");
+            return true;
+        } else if (oppoScore == 50) {
+            System.out.println("Player Two wins the Game");
+            return true;
+        } else {
+            return false;
+        }
     }
-//plays out a multi-round game, printing out the results of each round, the final score, and the overall winner (if there is one).
+
+    //plays out a multi-round game, printing out the results of each round, the final score, and the overall winner (if there is one).
     public void playGame(){
         throw new UnsupportedOperationException();
     }
